098348c078bdf9f9699d6b777234fd29a3388986
pocketdown <junghunjoo@icloud.com>
2026-09-23 20:07:29 +0900

    Update touch.sh

--- touch.sh
+++ touch.sh
@@ -1,3 +1,3 @@
 # dtoverlay=waveshare28a-v2;rotate=270
 # TransformationMatrix x++ yx-- x+ xy++ y++ y+ xy-- yx++ 1
-sudo sed -i '/touchscreen/,/"libinput"/s/.*Driver "libinput"/&\n        Option "TransformationMatrix" "1.1 0 0 0 -1.4 1.1 0 0 1"/' /usr/share/X11/xorg.conf.d/40-libinput.conf
+sudo sed -i '/touchscreen/,/"libinput"/s/.*"libinput"/&\n        Option "TransformationMatrix" "1.1 0 0 0 -1.4 1.1 0 0 1"/' /usr/share/X11/xorg.conf.d/40-libinput.conf
